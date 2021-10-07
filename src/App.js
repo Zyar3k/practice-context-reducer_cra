07202b066d02f9b13d177ac764734ffec145c4ca
zyar3k <a.zygan@gmail.com>
2021-10-07 23:20:11 +0200

    Prepare basic temp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Switch } from "react-router";
+
+import Heading from "./components/Heading";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Practice</h1>
+    <div>
+      <Heading />
+      <Switch>
+        <Route path="/" component={TaskList} exact />
+        <Route path="/add" component={TaskForm} />
+      </Switch>
     </div>
   );
 }
