47165e478cc13d65df002acfd49edc5a9c24a25e
zyar3k <a.zygan@gmail.com>
2021-10-07 23:34:02 +0200

    Add header & react router dom

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Heading />
-      <Switch>
-        <Route path="/" component={TaskList} exact />
-        <Route path="/add" component={TaskForm} />
-      </Switch>
+    <div className="h-screen text-white text-center p-10">
+      <div className="container mx-auto h-full">
+        <Heading />
+        <Switch>
+          <Route path="/" component={TaskList} exact />
+          <Route path="/add" component={TaskForm} />
+        </Switch>
+      </div>
     </div>
   );
 }
