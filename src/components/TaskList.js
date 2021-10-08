40b5252d0171231f14732900c686855ad528c36c
zyar3k <a.zygan@gmail.com>
2021-10-08 14:51:13 +0200

    Add context to project & render list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 const TaskList = () => {
-  return <div>TaskList</div>;
+  const { tasks } = useContext(GlobalContext);
+  console.log(tasks);
+  return (
+    <div className="flex justify-center">
+      <div className="w-6/12">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className="bg-gray-900 px-20 py-5 text-white shadow-2x1 mb-4 flex justify-between"
+          >
+            <div>
+              <h1>{task.title}</h1>
+              <h6>{task.id}</h6>
+            </div>
+            <div>
+              <button>EDIT</button>
+              <button>DELETE</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
---
+++ src/context/GlobalContext.js
@@ -0,0 +1,24 @@
+import React, { createContext } from "react";
+
+export const GlobalContext = createContext({
+  tasks: [
+    {
+      id: "1",
+      title: "Title 1",
+      description: "Some description",
+      done: false,
+    },
+    {
+      id: "2",
+      title: "Title 2",
+      description: "Another description",
+      done: false,
+    },
+    {
+      id: "3",
+      title: "Title 3",
+      description: "Different description",
+      done: true,
+    },
+  ],
+});
